42e28f5eea18619281b218b0f3861c72b00e49ab
playatanu <playatanu@gmail.com>
2022-03-09 12:01:35 +0530

    database url

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectdb = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/instPay');
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log('db connected');
   } catch (error) {
     throw error;
