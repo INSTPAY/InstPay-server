a06957f8fde2a8cef9b5583f451eb42cf4e59987
playatanu <playatanu@gmail.com>
2022-03-09 13:08:07 +0530

    callback url fix

--- controlers/googleAuth.js
+++ controlers/googleAuth.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/google/callback',
+      callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
       // create jwt token
