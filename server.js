1df3c323a9b95df9dc9847c1b804c9330f526c42
playatanu <playatanu@gmail.com>
2022-03-09 11:51:17 +0530

    database

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const passport = require('passport');
+const connectdb = require('./database');
 const env = require('dotenv');
 const app = express();
 
+// env config
 env.config();
+// database config
+connectdb();
 
+// Midelewears
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
+// routes
 app.get('/', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
 
 app.use('/auth', require('./routes/authRoutes'));
 
+// Listening port
 app.listen(80);
