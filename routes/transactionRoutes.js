cd43e8b846dac527072c48b09f99dae8f86e3391
playatanu <playatanu@gmail.com>
2022-03-13 16:57:11 +0530

    💁 get transaction by Trans id

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -6,9 +6,14 @@ router.post('/send', (req, res) => {
   transaction.pay(req, res);
 });
 
-//send money
+// All transactions
 router.post('/', (req, res) => {
   transaction.transactions(req, res);
+});
+
+// transactionById
+router.post('/:id', (req, res) => {
+  transaction.transactionById(req, res);
 });
 
 module.exports = router;
