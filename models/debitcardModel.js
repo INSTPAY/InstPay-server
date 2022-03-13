6206b8e8d3f6eaf34561f5bd3a54b97562e05fcc
playatanu <playatanu@gmail.com>
2022-03-13 17:12:20 +0530

    init debit card Model 🐪

---
+++ models/debitcardModel.js
@@ -0,0 +1,10 @@
+// const mongoose = require('mongoose');
+
+// const debitcardSceama = mongoose.Schema(
+//   {
+//
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model('Debitcard ', debitcardSceama);
