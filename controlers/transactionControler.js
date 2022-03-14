44376efdd71b6e043f9498f44ad9ba55e5f312dd
playatanu <playatanu@gmail.com>
2022-03-14 20:49:26 +0530

    change id toi _id 😄

--- controlers/transactionControler.js
+++ controlers/transactionControler.js
@@ -50,9 +50,9 @@ exports.transactions = async (req, res) => {
 };
 
 exports.transaction = async (req, res) => {
-  const { id } = req.body;
+  const { _id } = req.body;
   try {
-    const trans = await Transaction.findById(id);
+    const trans = await Transaction.findById(_id);
 
     if (trans) res.status(200).json(trans);
     else res.status(400).json({ message: 'transaction not found' });
