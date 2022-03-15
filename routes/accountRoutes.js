93b3fbf0703743855ad5b666000812fab25693ff
playatanu <playatanu@gmail.com>
2022-03-15 22:11:39 +0530

    de

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -13,6 +13,11 @@ router.patch('/', tokenAuth, (req, res) => {
   account.patch(req, res);
 });
 
+// reset pin
+router.patch('/', tokenAuth, (req, res) => {
+  account.resetPin(req, res);
+});
+
 // delete account
 router.delete('/', tokenAuth, (req, res) => {
   account.delete(req, res);
