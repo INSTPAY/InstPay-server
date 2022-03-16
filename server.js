30660ed7885cad76e1b30483a7d95709db7a0ed1
playatanu <playatanu@gmail.com>
2022-03-16 16:57:43 +0530

    server fix 1.0.2

--- server.js
+++ server.js
@@ -4,22 +4,6 @@ const connectdb = require('./database');
 const env = require('dotenv');
 const app = express();
 
-//websockets
-const WebSocket = require('ws');
-
-const server = require('http').createServer(app);
-const wss = new WebSocket.Server({ server: server });
-
-wss.on('connection', (ws, req) => {
-  ws.id = req.headers['account'];
-
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      if (client.id == '850567575') client.send('im fom account 850567575');
-    }
-  });
-});
-
 //const sendSms = require('./services/sendSms').default;
 
 // env config
@@ -57,21 +41,5 @@ app.use('/upload', require('./routes/uploadRoutes'));
 
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-app.get('/ws/:id', (req, res) => {
-  const id = req.params.id;
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      if (client.id == id)
-        client.send(
-          JSON.stringify({
-            id: id,
-            event: 'create ac',
-          })
-        );
-    }
-  });
-  res.send('sending ' + id);
-});
-
 // Listening port
-server.listen(8080);
+app.listen(8080);
