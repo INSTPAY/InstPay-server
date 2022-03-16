b6d1955f1fb2ef7e26a40498c174a704081fa127
playatanu <playatanu@gmail.com>
2022-03-16 23:01:52 +0530

    transation with add

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -16,4 +16,14 @@ router.post('/transaction', tokenAuth, (req, res) => {
   transaction.transaction(req, res);
 });
 
+// transaction with
+router.post('/transactions/with', tokenAuth, (req, res) => {
+  transaction.transactionWith(req, res);
+});
+
+// transaction with
+router.post('/transaction/with', tokenAuth, (req, res) => {
+  transaction.transactionWithId(req, res);
+});
+
 module.exports = router;
