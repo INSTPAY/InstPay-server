10848c2c0ed8817612382691ceaedec018ee510f
playatanu <playatanu@gmail.com>
2022-03-18 21:48:58 +0530

    payees trans by date

--- controlers/transactionControler.js
+++ controlers/transactionControler.js
@@ -79,7 +79,14 @@ exports.payessTransaction = async (req, res) => {
     const trans2 = await Transaction.find({ from: payee, to: account });
 
     const newtrans = [...trans, ...trans2];
-    if (newtrans) res.status(200).json(newtrans);
+
+    var byDate = newtrans.slice(0);
+    byDate.sort(function (a, b) {
+      return a.createdAt - b.createdAt;
+    });
+
+    console.log(byDate);
+    if (newtrans) res.status(200).json(byDate);
     else res.status(400).json({ message: 'transaction not found' });
   } catch (error) {
     res.status(500).json({ message: error.message });
