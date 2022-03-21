5c6a6e122088ae99834fc6c946c633331df35188
playatanu <playatanu@gmail.com>
2022-03-21 22:29:31 +0530

    verify Otp user add

--- controlers/verifyControler.js
+++ controlers/verifyControler.js
@@ -1,8 +1,18 @@
 const sendMail = require('../services/sendEmail');
 const Otp = require('../models/otpModel');
+const User = require('../models/userModel');
 
 exports.emailVerify = async (req, res) => {
   const { email } = req.body;
+
+  try {
+    const resUser = await User.findOne({ email: email });
+
+    if (resUser)
+      res.status(200).json({ message: 'email already', status: false });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 
   const otp = Math.floor(100000 + Math.random() * 900000);
 
@@ -27,7 +37,7 @@ exports.emailVerify = async (req, res) => {
   try {
     const resOtp = await newOtp.save();
     if (resOtp) resMail = await sendOtp();
-    if (resMail) res.status(200).json({ message: 'email sent' });
+    if (resMail) res.status(200).json({ message: 'email sent', status: true });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
